80555adaa30032c13908e2b9f2afc34f1d549bb4
Paolo Izzo <pizzo@subseacraft.com>
2024-12-27 16:37:27 +0100

    added display support

--- midi_program_controller/main.py
+++ midi_program_controller/main.py
@@ -1,20 +1,32 @@
 import picozero
-from machine import Timer
+import tm1637
+from machine import Pin, Timer
+from enum import Enum
+
+
+class DisplayState(Enum):
+    PAGE = 0
+    MIDI_PROGRAM = 1
+    MIDI_CHANNEL = 2
 
 
 class HardwareManager:
     def __init__(self) -> None:
+        # Buttons
         self.b_0 = picozero.Button(14)
         self.b_1 = picozero.Button(15)
         self.b_2 = picozero.Button(16)
         self.b_page_down = picozero.Button(17)
         self.b_page_up = picozero.Button(19)
         self.b_cfg = picozero.Button(20)
+        # LEDs
         self.led_0 = picozero.LED(24)
         self.led_1 = picozero.LED(25)
         self.led_2 = picozero.LED(26)
+        # Display
+        self.tm = tm1637.TM1637(clk=Pin(31), dio=Pin(32))
 
-    def set_patch_led(self, id: int):
+    def set_patch_led(self, id: int = -1):
         self.led_0.off()
         self.led_1.off()
         self.led_2.off()
@@ -25,6 +37,15 @@ class HardwareManager:
                 self.led_1.on()
             case 2:
                 self.led_2.on()
+
+    def display_page(self, page: int):
+        self.tm.number(page)
+
+    def display_program(self, program: int):
+        self.tm.show(f"P{program:3}")
+
+    def display_channel(self, channel: int):
+        self.tm.show(f"C{channel:3}")
 
 
 class MidiProgramManager:
@@ -46,8 +67,11 @@ class MidiProgramManager:
         self.page += delta
         self.update_program()
 
+    def update_channel(self, delta: int):
+        self.midi_channel += delta
+
     def update_program(self):
-        self.program = self.page * 3 + self.patch
+        self.midi_program = self.page * 3 + self.patch
 
 
 class MidiProgramController:
@@ -56,6 +80,13 @@ class MidiProgramController:
         self.pm = MidiProgramManager()
 
         self.send_timer = Timer()
+        # The display refreshes at a constant
+        self.display_refresh_timer = Timer()
+        self.display_refresh_timer.init(
+            mode=Timer.PERIODIC, freq=60, callback=self.display_refresh_timer
+        )
+
+        self.display_state = DisplayState.PAGE
         self.midi_program = 0
         self.page = 0
         self.patch = 0
@@ -65,21 +96,49 @@ class MidiProgramController:
         self.hw.b_2.when_activated = lambda: self.patch_update(2)
         self.hw.b_page_down.when_activated = lambda: self.page_update(-1)
         self.hw.b_page_up.when_activated = lambda: self.page_update(1)
+        self.hw.b_cfg.when_activated = self.cfg_on
+        self.hw.b_cfg.when_deactivated = self.cfg_off
+
+    def send_midi_program_change(self):
+        # Show the midi program being sent for a brief period
+        self.display_state = DisplayState.MIDI_PROGRAM
+        self.send_timer.deinit()
+        self.send_timer.init(mode=Timer.ONE_SHOT, period=100, callback=self.cfg_off)
+
+        # Send the midi program change request
+
+    def refresh_display(self):
+        match self.display_state:
+            case DisplayState.PAGE:
+                self.hw.display_page(self.pm.page)
+            case DisplayState.MIDI_PROGRAM:
+                self.hw.display_program(self.pm.midi_program)
+            case DisplayState.MIDI_CHANNEL:
+                self.hw.display_channel(self.pm.midi_channel)
 
     def patch_update(self, patch_number: int):
+        if self.hw.b_cfg.is_active:
+            return
         self.pm.set_patch(patch_number)
         self.hw.set_patch_led(patch_number)
         self.send_midi_program_change()
 
     def page_update(self, delta: int):
-        self.pm.update_page(delta)
+        if self.hw.b_cfg.is_active:
+            self.pm.update_channel(delta)
+        else:
+            self.display_state = DisplayState.PAGE
+            self.pm.update_page(delta)
+        # Delay the prgram change message a bit, in case you are searching through the pages
         self.send_timer.init(
             period=500, mode=Timer.ONE_SHOT, callback=self.send_midi_program_change
         )
 
-    def send_midi_program_change(self):
-        self.send_timer.deinit()
-        pass
+    def cfg_on(self):
+        self.display_state = DisplayState.MIDI_CHANNEL
+
+    def cfg_off(self):
+        self.display_state = DisplayState.PAGE
 
 
 while True:
