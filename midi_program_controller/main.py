25b45f0ce20708c4e257caeb3b075d67c16c2d52
Paolo Izzo <pizzo@subseacraft.com>
2024-12-30 16:57:43 +0100

    set boundaries and better page callback

--- midi_program_controller/main.py
+++ midi_program_controller/main.py
@@ -1,11 +1,7 @@
 import tm1637
 from machine import Pin, Timer, PWM
 from enum import Enum
-
-
-def pwm_duty_pct(pct: float) -> float:
-    return 65025 * pct * 0.01
-
+import math
 
 patch_btn = [
     Pin(14, Pin.IN, Pin.PULL_DOWN),
@@ -18,10 +14,15 @@ btn_cfg = Pin(20, Pin.IN, Pin.PULL_DOWN)
 patch_led = [PWM(Pin(24, Pin.OUT)), PWM(Pin(25, Pin.OUT)), PWM(Pin(26, Pin.OUT))]
 disp = tm1637.TM1637(clk=Pin(31), dio=Pin(32))
 
+n_pages = math.ceil(127 / len(patch_btn))
 
-class UpdateValue(Enum):
-    DOWN = -1
-    UP = 1
+
+def pwm_duty_pct(pct: float) -> float:
+    return 65025 * pct * 0.01
+
+
+def clamp(value: int | float, lower_bound: int | float, upper_bound: int | float) -> int | float:
+    return max(min(value, upper_bound), lower_bound)
 
 
 class State(Enum):
@@ -35,28 +36,30 @@ class State(Enum):
 
 class MidiProgramManager:
     def __init__(self) -> None:
-        self.midi_program = 0
-        self.midi_channel = 0
-        self.page = 0
-        self.patch = 0
+        self.midi_program = int(0)
+        self.midi_channel = int(0)
+        self.page = int(0)
+        self.patch = int(0)
 
     def set_patch(self, patch_number: int):
         self.patch = patch_number
         self._update_program()
 
     def set_page(self, page_number: int):
+        if page_number < 0 or page_number > n_pages:
+            print(f"Page {page_number} request is out of bound")
+            return
         self.page = page_number
         self._update_program()
 
-    def update_page(self, value: UpdateValue):
-        self.page += value.value
-        self._update_program()
-
-    def update_channel(self, value: UpdateValue):
-        self.midi_channel += value.value
+    def set_channel(self, channel: int):
+        if channel < 0 or channel > 15:
+            print(f"Channel {channel} request is out of bound")
+            return
+        self.midi_channel = channel
 
     def _update_program(self):
-        self.midi_program = self.page * 3 + self.patch
+        self.midi_program = clamp(self.page * 3 + self.patch, 0, 127)
 
 
 class MidiProgramController:
@@ -74,10 +77,8 @@ class MidiProgramController:
         for idx, btn in enumerate(patch_btn):
             btn.irq(trigger=Pin.IRQ_RISING, handler=lambda: self.patch_callback(idx))
 
-        btn_page_up.irq(trigger=Pin.IRQ_RISING, handler=lambda: self.page_callback(UpdateValue.UP))
-        btn_page_down.irq(
-            trigger=Pin.IRQ_RISING, handler=lambda: self.page_callback(UpdateValue.DOWN)
-        )
+        btn_page_up.irq(trigger=Pin.IRQ_RISING, handler=lambda: self.page_update_callback(1))
+        btn_page_down.irq(trigger=Pin.IRQ_RISING, handler=lambda: self.page_update_callback(-1))
         btn_cfg.irq(trigger=Pin.IRQ_RISING, handler=setattr(self, "state", State.CONFIG))
         btn_cfg.irq(trigger=Pin.IRQ_FALLING, handler=setattr(self, "state", State.SEND_PC_MESSAGE))
 
@@ -89,12 +90,12 @@ class MidiProgramController:
         self.pm.set_patch(value)
         self.state = State.SEND_PC_MESSAGE
 
-    def page_callback(self, value: UpdateValue):
+    def page_update_callback(self, delta: int):
         if self.state is State.CONFIG:
-            self.pm.update_channel(value)
+            self.pm.set_channel(self.pm.midi_channel + delta)
         else:
+            self.pm.set_page(self.pm.page + delta)
             self.state = State.PAGE_CHANGE
-            self.pm.update_page(value)
 
     def send_midi_pc(self):
         pass
@@ -146,4 +147,5 @@ class MidiProgramController:
 
             case State.CONFIG:
                 self.send_timer.deinit()
-                disp.show(f"C{self.pm.midi_channel:3}")
+                # Add one to the Midi channel being displayed, as this seems to be quite common
+                disp.show(f"C{self.pm.midi_channel + 1:3}")
