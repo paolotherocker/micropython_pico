57e784740eb0b0f56b34a5766f69b3104f146ddb
Paolo Izzo <pizzo@subseacraft.com>
2025-01-03 18:32:48 +0100

    moved hardware values to the class

--- midi_program_controller/main.py
+++ midi_program_controller/main.py
@@ -3,20 +3,16 @@ from machine import Pin, Timer, PWM, UART
 from enum import Enum
 import math
 
-patch_btn = [
-    Pin(14, Pin.IN, Pin.PULL_DOWN),
-    Pin(15, Pin.IN, Pin.PULL_DOWN),
-    Pin(16, Pin.IN, Pin.PULL_DOWN),
-]
-btn_page_up = Pin(17, Pin.IN, Pin.PULL_DOWN)
-btn_page_down = Pin(19, Pin.IN, Pin.PULL_DOWN)
-btn_cfg = Pin(20, Pin.IN, Pin.PULL_DOWN)
-patch_led = [PWM(Pin(24, Pin.OUT)), PWM(Pin(25, Pin.OUT)), PWM(Pin(26, Pin.OUT))]
-send_led = Pin(27, Pin.OUT)
-disp = tm1637.TM1637(clk=Pin(31), dio=Pin(32))
-
+# Pins
+p_patch_btn = [14, 15, 16]
+p_patch_led = [24, 25, 26]
+p_page_up = 17
+p_page_down = 19
+p_cfg = 20
+p_send_led = 27
+# Other constants
 k_midi_uart_id = 0
-k_pages = math.ceil(127 / len(patch_btn))
+k_pages = math.ceil(127 / len(p_patch_btn))
 
 
 def pwm_duty(pct: float) -> float:
@@ -72,6 +68,15 @@ class MidiProgramManager:
 
 class MidiProgramController:
     def __init__(self) -> None:
+        # Hardware
+        self.patch_btn = [Pin(x, Pin.IN, Pin.PULL_DOWN) for x in p_patch_btn]
+        self.btn_page_up = Pin(p_page_up, Pin.IN, Pin.PULL_DOWN)
+        self.btn_page_down = Pin(p_page_down, Pin.IN, Pin.PULL_DOWN)
+        self.btn_cfg = Pin(p_cfg, Pin.IN, Pin.PULL_DOWN)
+        self.patch_led = [PWM(Pin(p, Pin.OUT)) for p in p_patch_led]
+        self.send_led = Pin(p_send_led, Pin.OUT)
+        self.disp = tm1637.TM1637(clk=Pin(31), dio=Pin(32))
+
         self.midi = Midi(k_midi_uart_id)
         self.pm = MidiProgramManager()
 
@@ -80,7 +85,7 @@ class MidiProgramController:
         self.send_timer = Timer()
         self.update_timer = Timer()
 
-        for led in patch_led:
+        for led in self.patch_led:
             led.freq(1000)
 
     def init(self, frequency: int):
@@ -88,17 +93,19 @@ class MidiProgramController:
 
         :param int frequency: frequency at which the state machine and display are updated
         """
-        for idx, btn in enumerate(patch_btn):
+        for idx, btn in enumerate(self.patch_btn):
             btn.irq(trigger=Pin.IRQ_RISING, handler=lambda p: self.patch_press_callback(idx))
             btn.irq(
                 trigger=Pin.IRQ_FALLING,
                 handler=self.patch_release_callback,
             )
 
-        btn_page_up.irq(trigger=Pin.IRQ_RISING, handler=lambda p: self.page_update_callback(1))
-        btn_page_down.irq(trigger=Pin.IRQ_RISING, handler=lambda p: self.page_update_callback(-1))
-        btn_cfg.irq(trigger=Pin.IRQ_RISING, handler=lambda p: setattr(self, "state", State.CONFIG))
-        btn_cfg.irq(
+        self.btn_page_up.irq(trigger=Pin.IRQ_RISING, handler=self.page_up_callback)
+        self.btn_page_down.irq(trigger=Pin.IRQ_RISING, handler=self.page_down_callback)
+        self.btn_cfg.irq(
+            trigger=Pin.IRQ_RISING, handler=lambda p: setattr(self, "state", State.CONFIG)
+        )
+        self.btn_cfg.irq(
             trigger=Pin.IRQ_FALLING, handler=lambda p: setattr(self, "state", State.SEND_PC_MESSAGE)
         )
 
@@ -113,15 +120,24 @@ class MidiProgramController:
         if self.state is State.PROGRAM_CHANGE_DISP:
             self.state = State.IDLE
 
-    def page_update_callback(self, delta: int):
+    def page_up_callback(self, p: Pin):
         if self.state is State.CONFIG:
-            self.midi.set_channel(self.midi.channel + delta)
+            self.midi.set_channel(self.midi.channel + 1)
         else:
-            self.pm.set_page(self.pm.page + delta)
+            self.pm.set_page(self.pm.page + 1)
             self.state = State.PAGE_CHANGE
 
-    def send_midi_pc(self):
-        pass
+    def page_down_callback(self, p: Pin):
+        if self.state is State.CONFIG:
+            self.midi.set_channel(self.midi.channel - 1)
+        else:
+            self.pm.set_page(self.pm.page - 1)
+            self.state = State.PAGE_CHANGE
+
+    def set_patch_led(self, brightness: float):
+        for led in self.patch_led:
+            led.duty_u16(0)
+        self.patch_led[self.pm.patch].duty_u16(pwm_duty(brightness))
 
     def update_callback(self, timer: Timer):
         led_brightness = 100.0
@@ -129,10 +145,10 @@ class MidiProgramController:
         # Refresh display
         match self.state:
             case State.IDLE:
-                disp.number(self.pm.page)
+                self.disp.number(self.pm.page)
 
             case State.PAGE_CHANGE:
-                disp.number(self.pm.page)
+                self.disp.number(self.pm.page)
 
                 # Go into wait mode and trigger a send state after a short period
                 self.state = State.PAGE_CHANGE_WAIT
@@ -144,35 +160,33 @@ class MidiProgramController:
 
             case State.PAGE_CHANGE_WAIT:
                 led_brightness = 25.0
-                disp.number(self.pm.page)
+                self.disp.number(self.pm.page)
 
             case State.SEND_PC_MESSAGE:
                 self.send_timer.deinit()
                 self.midi.write_program_change(self.pm.program)
 
                 # Blink the send LED once
-                send_led.on()
+                self.send_led.on()
                 self.send_timer.init(
-                    mode=Timer.ONE_SHOT, period=250, callback=lambda t: send_led.off()
+                    mode=Timer.ONE_SHOT, period=250, callback=lambda t: self.send_led.off()
                 )
 
                 # Go into program display mode and trigger the idle state after a short period
                 self.state = State.PROGRAM_CHANGE_DISP
 
             case State.PROGRAM_CHANGE_DISP:
-                disp.show(f"P{self.pm.program:3}")
+                self.disp.show(f"P{self.pm.program:3}")
 
             case State.CONFIG:
                 self.send_timer.deinit()
 
                 led_brightness = 25.0
                 # Add one to the Midi channel being displayed, as this seems to be quite common
-                disp.show(f"C{self.midi.channel + 1:3}")
+                self.disp.show(f"C{self.midi.channel + 1:3}")
 
         # Update LED states
-        for led in patch_led:
-            led.duty_u16(0)
-        patch_led[self.pm.patch].duty_u16(pwm_duty(led_brightness))
+        self.set_patch_led(led_brightness)
 
 
 midi_pc = MidiProgramController()
