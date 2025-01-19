77ec39d7c3fface2eee36ecdd51c611a84ba5a3b
Paolo Izzo <Paolo Izzo>
2025-01-19 17:47:00 +0000

    improved tm1637 test

--- test_tm1637/main.py
+++ test_tm1637/main.py
@@ -1,22 +1,28 @@
 import tm1637
 from machine import Pin
 
+k_led_pin = int(25)
+k_btn_pin = int(16)
+
 
 class Test_tm1637:
     def __init__(self) -> None:
         self.disp = tm1637.TM1637(clk=Pin(26), dio=Pin(27))
-        self.led = Pin(25, Pin.OUT)
+        self.led = Pin(k_led_pin, Pin.OUT)
         self.value = 0
-        self.btn = Pin(16, Pin.IN, Pin.PULL_UP)
+        self.btn = Pin(k_btn_pin, Pin.IN, Pin.PULL_UP)
 
         self.disp.brightness(3)
         self.btn.irq(handler=self.button_callback)
 
         self.led.off()
-        self.disp.show("P   ")
+        self.disp.show(f"P{self.value:3}")
 
     def button_callback(self, pin: Pin):
         self.value += 1
+        if self.value > 127:
+            self.value = 0
+
         self.disp.show(f"P{self.value:3}")
 
         if self.btn.value() is 0:
