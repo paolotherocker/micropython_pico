3669e83c81412c90e5f29f553c8d47d4fd18b4b0
Paolo Izzo <Paolo Izzo>
2025-01-19 17:58:12 +0000

    enum like class in led_fade

--- led_fade/main.py
+++ led_fade/main.py
@@ -1,6 +1,10 @@
 from machine import Pin, Timer, PWM
 
-State: dict[str, int] = {"UP": 0, "DOWN": 1}
+
+class State:
+    UP = 1
+    DOWN = 2
+
 
 k_pwm_max = 65025
 
@@ -15,20 +19,20 @@ class LedFader:
         self.brightness = 0.0
         self.timer = Timer()
         self.led = PWM(Pin(25, Pin.OUT))
-        self.state = State["UP"]
+        self.state = State.UP
 
         self.led.freq(1000)
         self.timer.init(period=5, mode=Timer.PERIODIC, callback=self.update)
 
     def update(self, timer):
         if self.brightness >= 1:
-            self.state = State["DOWN"]
+            self.state = State.DOWN
         elif self.brightness <= 0:
-            self.state = State["UP"]
+            self.state = State.UP
 
-        if self.state is State["UP"]:
+        if self.state is State.UP:
             self.brightness += 0.01
-        elif self.state is State["DOWN"]:
+        elif self.state is State.DOWN:
             self.brightness -= 0.01
 
         self.led.duty_u16(pwm_duty(self.brightness))
